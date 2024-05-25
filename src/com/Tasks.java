ed324f6d459b801db5f3706e5934dd7af9ab5676
KamilNabiullin <kamil.nabiullin.05@mail.ru>
2024-05-25 13:33:51 +0300

    .

---
+++ src/com/Tasks.java
@@ -0,0 +1,32 @@
+package com;
+
+import com.utils.*;
+
+import java.util.Queue;
+import java.util.Random;
+
+public class Tasks  {
+    private String nameOFTask;
+    private int durationOfTaskInMinutes;
+
+    @Override
+    public String toString() {
+        return "Tasks{" +
+                "nameOFTask='" + nameOFTask + '\'' +
+                ", durationOfTaskInMinutes=" + durationOfTaskInMinutes +
+                '}';
+    }
+
+    public int getDurationOfTaskInMinutes() {
+        return durationOfTaskInMinutes;
+    }
+
+    public void setDurationOfTaskInMinutes(int durationOfTaskInMinutes) {
+        this.durationOfTaskInMinutes = durationOfTaskInMinutes;
+    }
+
+    public Tasks(String nameOFTask, int durationOfTask) {
+        this.nameOFTask = nameOFTask;
+        this.durationOfTaskInMinutes = durationOfTask;
+    }
+}
